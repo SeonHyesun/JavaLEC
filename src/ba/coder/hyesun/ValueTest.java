9152b06aaeac2f36634fa0ce34e4dcee6172e186
bacoder <bacoder@DESKTOP-PV1DTRS>
2018-11-26 21:54:35 +0900

    init

---
+++ src/ba/coder/hyesun/ValueTest.java
@@ -0,0 +1,25 @@
+package ba.coder.hyesun;
+
+public class ValueTest {
+	int GlobalVariable = 5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Javascript practice"+"2");
+		
+		int aVal = 10;
+		int bVal = 5;
+		int Tot;
+		
+		Tot=aVal+bVal;
+		System.out.println("\"Total\"="+Tot);
+
+		
+	}
+		
+
+	
+	}
+
+}
